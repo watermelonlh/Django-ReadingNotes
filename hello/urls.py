984826f4dd31c02f6f13bcd43fe482e13cd01846
watermelon <watermelonlh@gmail.com>
2013-05-29 01:27:21 +0800

    change reading_notes url

--- hello/urls.py
+++ hello/urls.py
@@ -16,5 +16,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'static/'}),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^reading/', include('reading_notes.urls')),
+    url(r'^$', include('reading_notes.urls')),
 )
