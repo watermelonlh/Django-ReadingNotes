322883d71e4b1854f810363704dc984e1e4bafe2
watermelon <watermelonlh@gmail.com>
2013-05-29 00:35:37 +0800

    edit setting static file dir

--- hello/settings.py
+++ hello/settings.py
@@ -71,8 +71,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    ('bootstrap', "/home/watermelon/DjangoOnHeroku/static_files/bootstrap"),
-    ('admin', "/home/watermelon/DjangoOnHeroku/static_files/admin"),
+    ('bootstrap', "static_files/bootstrap"),
+    ('admin', "static_files/admin"),
 )
 
 # List of finder classes that know how to find static files in
