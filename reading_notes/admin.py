5f7561f00b633b44b6924b0f53c4dbcb69c9c445
watermelon <watermelonlh@gmail.com>
2013-05-29 01:18:27 +0800

    add reading_notes/admin.py

---
+++ reading_notes/admin.py
@@ -0,0 +1,7 @@
+from ReadingNotes.models import ReadingType, Reading, Note
+from django.contrib import admin
+
+admin.site.register(ReadingType)
+admin.site.register(Reading)
+admin.site.register(Note)
+
