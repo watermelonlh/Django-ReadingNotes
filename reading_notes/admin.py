00639a13ffc07e65f7131a3b15e53946d56575c0
watermelon <watermelonlh@gmail.com>
2013-05-29 01:20:25 +0800

    edit admin.py

--- reading_notes/admin.py
+++ reading_notes/admin.py
@@ -1,4 +1,4 @@
-from ReadingNotes.models import ReadingType, Reading, Note
+from reading_notes.models import ReadingType, Reading, Note
 from django.contrib import admin
 
 admin.site.register(ReadingType)
