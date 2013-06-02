762cdd1c3c71a7fde3cf0e41ac773d0fc4b4e518
watermelon <watermelonlh@gmail.com>
2013-06-02 14:21:35 +0800

    ignore settings.py, #6, #10

--- reading_notes/models.py
+++ reading_notes/models.py
@@ -1,4 +1,6 @@
 import time
+import datetime
+from django.utils.timezone import utc
 from django.db import models
 
 # Create your models here.
@@ -10,13 +12,11 @@ class ReadingType(models.Model):
 
 
 class Reading(models.Model):
-    title = models.CharField(max_length=1024)
+    title = models.CharField(max_length=1024, unique=True)
     author = models.CharField(max_length=1024, null=True, blank=True)
     reading_type = models.ForeignKey(ReadingType)
+    update_date = models.DateTimeField('date updated', default=datetime.datetime.utcnow().replace(tzinfo=utc))
 
-    class Meta:
-        unique_together = ('title', 'author')
-    
     def __unicode__(self):
         return "[" + self.reading_type.name + "]" + self.title
 
