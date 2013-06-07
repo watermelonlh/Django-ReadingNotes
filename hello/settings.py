369e9f9e2988e6dcf67f8ac727c6c9bcf144b18b
unknown <t-guyliu@ccic-3496305.fareast.corp.microsoft.com>
2013-06-07 11:01:41 +0800

    change setting.py

--- hello/settings.py
+++ hello/settings.py
@@ -71,8 +71,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    ('bootstrap', "static_files/bootstrap"),
-    ('admin', "static_files/admin"),
+    ('', "static_files/"),
 )
 
 # List of finder classes that know how to find static files in
